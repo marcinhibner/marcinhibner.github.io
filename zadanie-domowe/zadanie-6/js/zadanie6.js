3ed63bb1b3285d4fc16e6d6019a757d9360893ba
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-13 18:18:36 +0100

    zadanie6

---
+++ zadanie-domowe/zadanie-6/js/zadanie6.js
@@ -0,0 +1,18 @@
+var tekst = ["A", "k", "a", "d", "e", "m", "i", "a", "1", "0", "8"];
+console.log(tekst);
+
+var tekstReverse = tekst.reverse();
+console.log(tekstReverse);
+
+var tablicaTekst = tekst.join("");
+console.log(tablicaTekst);
+
+
+
+// function reverseString(tekst) {
+//     var tesktOdwrocony = tekst.reverse();
+//     var tekstPolaczony = tekst.join("");
+// }
+
+// var odwroconyTekst = reverseString();
+// console.log(odwroconyTekst);
