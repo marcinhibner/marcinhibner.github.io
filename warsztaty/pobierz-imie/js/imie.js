ac881c4a62076432396398c4bff53f1e0e9fb0d9
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-17 21:53:50 +0100

    zmiany

---
+++ warsztaty/pobierz-imie/js/imie.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function pobierzImie(event) {
+    event.preventDefault();
+    console.log("dziala :)");
+    var form = document.getElementById("formularz");
+    //pobrac imie i nazwisko ???
+}
---
+++ warsztaty/ustaw-tlo/js/tlo.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function ustawTlo() {
+    var allParagraphs = document.getElementsByTagName("p");
+   // console.log(allParagraphs);
+    allParagraphs[0].style.backgroundColor = "red";
+    allParagraphs[1].style.backgroundColor = "yellow";
+}
+    
+
+    
