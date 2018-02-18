699a47e95fc9edf4942c04cf357014518b7f85dd
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-18 16:58:42 +0100

    walidacja-cd

--- warsztaty/walidacja-formularza/js/main.js
+++ warsztaty/walidacja-formularza/js/main.js
@@ -21,8 +21,41 @@ function stanCheckboxa() {
     zgodaMarketingowa2.checked = this.checked;
     
     zgodaMarketingowa1.disabled = this.checked;
-    zgodaMarketingowa2.disabled = this.checked;
-    
+    zgodaMarketingowa2.disabled = this.checked;  
 }
 
 wszystkieZgody.onchange = stanCheckboxa;
+
+
+// to na czym nie byles
+
+function walidujFormularz(event) {
+    if (inputImie.value == "")
+//        console.log("Dziala");
+        var msgImie = document.createElement("li");
+        msgImie.innerHTML = "Wpisz imie";
+        wiadomosc.appendChild(msgImie);
+        event.preventDefault();
+    
+    if (inputEmail.value == "")
+//        console.log("Dziala");
+        var msgEmail = document.createElement("li");
+        msgEmail.innerHTML = "Wpisz email";
+        wiadomosc.appendChild(msgEmail);
+        event.preventDefault();
+}
+
+   if (!zgodaMarketingowa1.checked) {
+        var msgZgoda = document.createElement("li");
+        msgZgoda.innerHTML = "Musisz wyrazic zgode";
+        wiadomosc.appendChild(msgZgoda);
+        event.preventDefault();
+   
+}
+
+//wyslijBtn.addEventListener("click", walidujFormularz);  lub
+wyslijBtn.onclick = walidujFormularz;
+
+
+
+
