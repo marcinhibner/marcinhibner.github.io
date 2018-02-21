dd662c96051abcadf9fdd3a432877cdaab33fa22
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-21 17:45:41 +0100

    poprawki-zadan-domowych

--- zadanie-domowe/zadanie-15/js/zadanie15.js
+++ zadanie-domowe/zadanie-15/js/zadanie15.js
@@ -2,25 +2,17 @@
 
 $( "button" ).click( function() {
  $( "div" ).animate( { 
-   right: '100px',
-   height: '100px',
-   width: '100px',
- }, "3000" );
+   "margin-left": '100px',
+   "height": '100px',
+   "width": '100px',
+}, 3000, function() {
+  $( "div" ).animate( {
+    "background-color": 'blue',
+}, 5000 ); 
 });
- 
-
-// function callbackZwrotny () {
-//   $("#kwadrat").animate({
-//       "margin-left": "100px"
-//       "height": "100px",
-//       "width": "100px",
-//   }, 3000);
-// };
+});
 
 
-//     callbackZwrotny();
-    
-//     $("#kwadrat").animate({
-//         "background-color": "blue",
-//     } , 5000, callbackZwrotny);
-// });
+// function() {
+//   $( "h2" ).show();
+// }
