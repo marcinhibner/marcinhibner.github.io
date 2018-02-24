97db52be6e1fe7b4b05c8db526ed91a57c34cc02
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-24 09:53:10 +0100

    powtorki-js

---
+++ warsztaty/powtorka-js/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var tekst = 'jakis tekst';
+
+//tworzenie znacznika
+var paragraf = document.createElement('p');
+
+document.getElementById('kontener').appendChild(paragraf);
+
+//paragraf.appendChild(tekst);
+
+paragraf.innerHTML = tekst;
+
+
+
+
+console.log(document.getElementById('string').innerHTML);
+
+
+var paragraf2 = document.createElement('p');
+
+document.body.appendChild(paragraf2);
+
+paragraf2.innerHTML = document.getElementById('string').innerHTML;
