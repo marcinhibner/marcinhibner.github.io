287aa9ea14954f70dd10407799a7797924a41270
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-24 14:58:27 +0100

    kalkulator-odsetek

---
+++ warsztaty/kalkulator-odsetek/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+                    
+function oblicz(event) {
+    event.preventDefault();
+    var wplata = parseInt(document.getElementById('wplata').value, 10);
+    console.log(wplata);
+}
