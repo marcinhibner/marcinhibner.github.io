20b0c332f31e45b89d83634d695194e0725c6d84
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-25 11:58:07 +0100

    warsztat-js-button-click

---
+++ warsztaty/js-button-click/js/main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+function ajax( method, url ) {
+    
+    
+    var httpReq = new XMLHttpRequest();
+    
+    
+    httpReq.open( method, url );
+    
+    
+    
+    httpReq.onreadystatechange = function() {
+        
+
+        if ( httpReq.readyState == 4 ) {
+            
+        
+            if ( httpReq.status == 200 ) {
+                
+                var returnData = httpReq.responseText;
+
+                console.log(returnData);
+                httpReq.onsuccess(returnData);
+            
+                httpReq = null;
+                
+            }
+            
+        }
+    
+    }
+
+
+httpReq.onsuccess = function(response) {
+        var jsonObj = JSON.parse(response);
+        console.log(jsonObj);
+        
+    var par1 = document.createElement('p');
+    var par2 = document.createElement('p');
+    var par3 = document.createElement('p');
+    
+    document.body.appendChild(par1);
+    document.body.appendChild(par2);
+    document.body.appendChild(par3);
+    
+    }
+
+    httpReq.send();
+}
+
+function pobierzDane() {
+    ajax( "GET", "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl" );
+}
+
+document.getElementById('pobierz').addEventListener('click', pobierzDane);
