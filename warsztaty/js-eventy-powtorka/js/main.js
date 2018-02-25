c8dee92c8f3936223906fcbfb608ca66a2b27ed0
Marcin Hibner <marcin.hibner@o2.pl>
2018-02-25 10:56:49 +0100

    powtorki-cd

--- warsztaty/js-eventy-powtorka/js/main.js
+++ warsztaty/js-eventy-powtorka/js/main.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', function() {
     console.log('123');
     
     var y = window.pageYOffset;
-    var headerHeight =document.getElementById('main-header').offsetHeight;
+    var headerHeight = document.getElementById('main-header').offsetHeight;
     
    if(y >= headerHeight) {
        document.getElementById('main-header').style.backgroundColor = 'yellow'
@@ -15,11 +15,20 @@ window.addEventListener('scroll', function() {
 window.addEventListener('scroll', function() {
     
     var y = window.pageYOffset;
-    var navHeight =document.getElementById('main-nav').offsetHeight;
-    
+    var navHeight = document.getElementById('main-nav').offsetHeight;
+    var headerHeight = document.getElementById('main-header').offsetHeight;
+
    if(y >= navHeight) {
        document.getElementById('main-nav').classList.add('transparent');
    } else {
        document.getElementById('main-nav').classList.remove('transparent');
    }
-   });
+    
+    if(y >= headerHeight) {
+        var bgPos = -((y - headerHeight) / 5);
+        
+        
+        document.getElementById('background').style.backgroundPosition = bgPos + 'px' + ' 0';
+    }
+});
+
